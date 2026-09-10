3519182101c6cb069a6472a980e9168145f07ad8
Philippe Laval <philippe.laval@qualiparc.net>
2026-09-10 16:03:44 +0200

    add empty files

---
+++ GraphSearch/Embeddings/IVectorIndex.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GraphSearch.Library.Embeddings
+{
+    public interface IVectorIndex
+    {
+    }
+}
---
+++ GraphSearch/Query/GraphRagResult.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GraphSearch.Library.Query
+{
+    internal class GraphRagResult
+    {
+    }
+}
